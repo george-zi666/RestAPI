f0c3cbb9e13bbc73d270cddb51c8e2ce8f9b5f35
george-zi666 <zibrovski.g@gmail.com>
2021-03-07 08:48:19 +0700

    first commit

---
+++ Models/Base/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Models.Base
+{
+    public class BaseModel
+    {
+    }
+}
